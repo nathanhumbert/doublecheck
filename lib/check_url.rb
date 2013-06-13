0b7225ab0fadd58ba660b08eabbd78a688681ed1
Daniel Gaytan <dgaytan6@gmail.com>
2013-06-13 17:33:36 -0500

    Fix bug: It was failing when the path is the root path in the URI. Probably a Rails issue.

--- lib/check_url.rb
+++ lib/check_url.rb
@@ -4,7 +4,12 @@ class DoubleCheck::CheckUrl
   def self.get_response(url)
     uri = URI.parse(url)
     response = Net::HTTP.start(uri.host, uri.port) do |http|
-      http.get(uri.path)
+      if uri.path.empty?
+        path = '/'
+      else
+        path = uri.path
+      end
+      http.get(path)
     end
   end
 end
